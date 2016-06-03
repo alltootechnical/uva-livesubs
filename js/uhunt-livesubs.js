b75b8da2d1d60d6fb6df2c9ee186f9c4faaca9fe
alltootechnical <free2sendmail@gmail.com>
2016-06-04 00:05:55 +0800

    Big changes
    Added clock, improved pending runs count and "queue zoned" warnings,
    etc.

--- js/uhunt-livesubs.js
+++ js/uhunt-livesubs.js
@@ -33,4 +33,17 @@ angular.module('uHunt.livesubs', ['uHunt.base'])
   }
 })
 
+function TimeCtrl($scope, $timeout) {
+    $scope.clock = Date.now();
+    $scope.tickInterval = 1000 //ms
+
+    var tick = function() {
+        $scope.clock = Date.now() // get the current time
+        $timeout(tick, $scope.tickInterval); // reset the timer
+    }
+
+    // Start the timer
+    $timeout(tick, $scope.tickInterval);
+}
+
 ;
